5143dd35903893768617b84891dba6929cf5187a
guerreroaanathalia@gmail.com <guerreroaanathalia@gmail.com>
2022-11-15 13:26:33 -0500

    boton cambio listo

--- controlador_cambio.js
+++ controlador_cambio.js
@@ -9,8 +9,10 @@ let etiquetaBoton=document.getElementById("boton")
 etiquetaBoton.addEventListener("click",function(evento){
     etiquetaImagen.src="./img/img1.jpg";
     etiquetaTitulo.textContent="Capitulo 1"
+    etiquetaTitulo.classList.add("color_titulo")
     etiquetaParrafo.textContent="De la mano de su creador, Masami Kurumada, nace en 1986 el primer volumen del manga shonen1 de Los Caballeros del Zodíaco (Saint Seiya), publicado en la revista Shonen Jump. Las productoras Bandai y Toei Animation, casi instantáneamente, lanzaron la adaptación animada en Japón de la famosa saga del Santuario. El éxito no tardó en propagarse en Occidente, así que durante las tres décadas siguientes el universo expandido de Los Caballeros… crece exponencialmente: secuelas, precuelas, spin-offs y numerosas películas basadas en los personajes (esta sería la sexta)."
-    etiquetaMenu.classList.add("color")
+    etiquetaParrafo.classList.add("color_letra")
+    etiquetaMenu.classList.add("color_menu")
     etiquetaFondo.classList.add("fondo")
     etiquetaAudio.src="./audio/audio1.mp3"
 })
